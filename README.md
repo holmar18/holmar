087f31674a42fb80dc076ad6174deddfe4101db0
OMar Omarsson <holmar17@ru.is>
2023-08-31 02:56:13 +0700

    Add Github serach assignment

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ### Check out my github.
 
 [Holmar Github](https://github.com/holmar18)
+
+##### https://github.com/gitname/react-gh-pages
--- src/components/portfolio/Projects-Data.js
+++ src/components/portfolio/Projects-Data.js
@@ -11,6 +11,7 @@ import TIPCALCULATOR from '../../assets/Projects/tip-calculator-fe.png';
 import RPS from '../../assets/Projects/rps.png';
 import IACARD from '../../assets/Projects/inter-card.jpg';
 import SPACETOUR from '../../assets/Projects/space-tourism.jpg';
+import GITSEARCH from '../../assets/Projects/GITSEARCH.jpg';
 
 export default [
 	{
@@ -68,17 +69,9 @@ export default [
 		github: false,
 		demo: 'https://akstur1.netlify.app/',
 	},
+
 	{
 		id: 7,
-		image: HANGMAN,
-		title: 'Hangman game',
-		yearMade: 'Jun 5, 2021',
-		descrition: 'Game I made just to play arround with animations',
-		github: 'https://github.com/holmar18/Hangman',
-		demo: 'https://hangman1988.netlify.app/',
-	},
-	{
-		id: 8,
 		image: BENSIN,
 		title: 'BensinVaktin',
 		yearMade: 'Jul 24, 2021',
@@ -87,7 +80,7 @@ export default [
 		demo: 'https://bensinvaktin.netlify.app/',
 	},
 	{
-		id: 9,
+		id: 8,
 		image: MYSOCCERSKILLS,
 		title: 'MySoccerSkills',
 		yearMade: 'Apr 30, 2022',
@@ -97,7 +90,7 @@ export default [
 		demo: 'https://fnd.io/#/is/iphone-app/1616905890-mysoccerskills-by-holmar-karl-holmarsson',
 	},
 	{
-		id: 10,
+		id: 9,
 		image: TIPCALCULATOR,
 		title: 'Tip Calculator',
 		yearMade: 'Oct 16, 2022',
@@ -106,7 +99,7 @@ export default [
 		demo: 'https://holmar-tip-calculator.netlify.app/',
 	},
 	{
-		id: 11,
+		id: 10,
 		image: RPS,
 		title: 'Rock Paper Scissor',
 		yearMade: 'Oct 18, 2022',
@@ -115,22 +108,31 @@ export default [
 		demo: 'https://holmar-rock-paper-scissor.netlify.app/',
 	},
 	{
-		id: 12,
+		id: 11,
 		image: IACARD,
 		title: 'Interactive card input',
 		yearMade: 'Oct 24, 2022',
 		descrition: 'Frontend Mentor challange - just for practice.',
 		github:
-			'https://github.com/holmar18/frontend-mentor-interactive-card-details-form',
+			'https://github.com/holmar18/holmar18-frontend-mentor-interactive-card-details-form',
 		demo: 'https://holmar-card-details.netlify.app/',
 	},
 	{
-		id: 13,
+		id: 12,
 		image: SPACETOUR,
 		title: 'Space tourism',
 		yearMade: 'Oct 31, 2022',
 		descrition: 'Frontend Mentor challange - just for practice.',
 		github: 'https://github.com/holmar18/frontend-mentor-space-tourism-website',
 		demo: 'https://holmar-space-tourism.netlify.app/',
+	},
+	{
+		id: 13,
+		image: GITSEARCH,
+		title: 'GitHub User Search',
+		yearMade: 'Nov 8, 2022',
+		descrition: 'Frontend Mentor challange - just for practice.',
+		github: 'https://github.com/holmar18/frontend-mentor-github-user-search',
+		demo: 'https://holmar-github-user-search.netlify.app/',
 	},
 ];
